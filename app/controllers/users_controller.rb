c46fde53f77284992bca3a36607bd4bac4137eb0
Kyumin Kim <starkying@gmail.com>
2017-08-14 03:22:09 +0000

    Add edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "울림, 회원가입이 완료되었습니다"
+      flash[:success] = "울림: 회원가입이 완료되었습니다!"
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "계정이 성공적으로 수정되었습니다!"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
   
